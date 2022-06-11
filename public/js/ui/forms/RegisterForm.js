69072e8c3ea5f4edfd6adfc37d9c759684d98417
Dmitry Votchitsev <dmitry.votchitsev@gmail.com>
2022-06-11 19:57:46 +0200

    RegisterForm create

--- public/js/ui/forms/RegisterForm.js
+++ public/js/ui/forms/RegisterForm.js
@@ -10,6 +10,14 @@ class RegisterForm extends AsyncForm {
    * и закрывает окно, в котором находится форма
    * */
   onSubmit(data) {
-
+    User.register(data, (err, response) => {
+      if (response.success) {
+        App.setState('user-logged');
+        this.element.reset();
+        App.getModal('register').close();
+        return
+      }
+      alert(response.error);
+    });
   }
 }
